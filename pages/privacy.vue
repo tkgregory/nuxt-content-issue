91d2f383137503d569ef63579ca0a61684e6e210
Tom Gregory <t.k.gregory@gmail.com>
2024-12-20 09:19:16 +0900

    Setup local demo.

---
+++ pages/privacy.vue
@@ -0,0 +1,13 @@
+<template>
+    <main>
+        <h1 class="text-3xl font-bold mb-8">Privacy</h1>
+
+        <div>
+            <NuxtLink to="/support" class="link link-hover">Support</NuxtLink> &nbsp;
+            <NuxtLink to="/privacy" class="link link-hover">Privacy policy</NuxtLink>
+        </div>
+
+        <ContentDoc path="/privacy" :head="false" />
+
+    </main>
+</template>
---
+++ pages/support.vue
@@ -0,0 +1,13 @@
+<template>
+    <main>
+        <h1 class="text-3xl font-bold mb-8">Support</h1>
+
+        <div>
+            <NuxtLink to="/support" class="link link-hover">Support</NuxtLink> &nbsp;
+            <NuxtLink to="/privacy" class="link link-hover">Privacy</NuxtLink>
+        </div>
+
+        <ContentDoc path="/support" :head="false" />
+
+    </main>
+</template>
