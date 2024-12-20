3ec21a26ecf054e292667628d82a4280b851e789
Tom Gregory <t.k.gregory@gmail.com>
2024-12-20 10:01:37 +0900

    Add deployment.

--- pages/support.vue
+++ pages/support.vue
@@ -3,8 +3,8 @@
         <h1 class="text-3xl font-bold mb-8">Support</h1>
 
         <div>
-            <NuxtLink to="/support" class="link link-hover">Support</NuxtLink> &nbsp;
-            <NuxtLink to="/privacy" class="link link-hover">Privacy</NuxtLink>
+            <NuxtLink to="/support">Support</NuxtLink> &nbsp;
+            <NuxtLink to="/privacy">Privacy</NuxtLink>
         </div>
 
         <ContentDoc path="/support" :head="false" />
